c4e74841140f817957bc20233a8b7fa67ff401af
Paul Stonebraker <wpstonebraker@gmail.com>
2021-06-12 13:00:57 -0400

    work on classes

--- chap2/src/app.ts
+++ chap2/src/app.ts
@@ -1,4 +1,60 @@
-const dad = "dad";
+class Department {
+  // private readonly id: string;
+  // public name: string;
+  private employees: string[] = [];
 
-console.log(dad);
-console.log("yo");
+  constructor(private readonly id: string, public name: string) {}
+
+  describe(this: Department) {
+    console.log(`Department ${this.id}: ${this.name}`);
+  }
+
+  // changeId(newId: string) {
+  //   this.id = newId;
+  // }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, "IT");
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "ACCOUNTING");
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment("69", ["Dad"]);
+
+it.addEmployee("Dad");
+it.addEmployee("Mom");
+
+it.printEmployeeInformation();
+it.describe();
+
+console.log(it);
+
+const acct = new AccountingDepartment("d2", []);
+
+acct.addReport("nooo");
+
+acct.printReports();
